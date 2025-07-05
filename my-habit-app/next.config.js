132168505d805b47104789554dbfd4144b2d1132
somniarch <156504751+somniarch@users.noreply.github.com>
2025-07-06 00:02:13 +0900

    next.config.js

--- my-habit-app/next.config.js
+++ my-habit-app/next.config.js
@@ -1,8 +1,8 @@
-// next.config.ts
+// next.config.js
 const nextConfig = {
   // Vercel에서는 별도 설정이 없어도 자동으로 최적화됩니다.
   // 필요하다면 기타 옵션만 추가
 };
 
-export default nextConfig;
+module.exports = nextConfig;
 
