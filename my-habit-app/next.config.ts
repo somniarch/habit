79cc0d5fc90bc9a266337ac872a7818fb691a059
somniarch <156504751+somniarch@users.noreply.github.com>
2025-07-05 10:43:07 +0900

    next.config.ts

--- my-habit-app/next.config.ts
+++ my-habit-app/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  basePath: "/habit", // 실제 레포지토리명으로 변경
+  assetPrefix: "https://somniarch.github.io/habit", // 실제 깃허브 아이디/레포지토리명으로 변경
+  // 기타 옵션
 };
 
 export default nextConfig;
