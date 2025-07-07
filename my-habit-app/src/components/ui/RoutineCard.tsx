57d17acefd8cd1e8c7cd1ceec9be4036df7d27c3
somniarch <156504751+somniarch@users.noreply.github.com>
2025-07-07 13:01:28 +0900

    RoutineCard.tsx

---
+++ my-habit-app/src/components/ui/RoutineCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function RoutineCard({ routine, onUpdate, onDelete, onRate, onEdit }: any) {
+  return (
+    <div className="routine-card">{/* 루틴 항목 UI */}</div>
+  );
+}
