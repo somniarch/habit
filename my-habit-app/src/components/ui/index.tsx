36fed7e43faedb1c008e8c036a1ee26236edee85
somniarch <156504751+somniarch@users.noreply.github.com>
2025-07-07 15:50:50 +0900

    index.tsx

--- my-habit-app/src/components/ui/index.tsx
+++ my-habit-app/src/components/ui/index.tsx
@@ -1,5 +1,15 @@
+// src/components/ui/index.tsx
+
 import AuthForm from './components/AuthForm';
 import RoutineCard from './components/RoutineCard';
 import HabitSuggestion from './components/HabitSuggestion';
 import StatisticsCharts from './components/StatisticsCharts';
 import DiaryView from './components/DiaryView';
+
+export {
+  AuthForm,
+  RoutineCard,
+  HabitSuggestion,
+  StatisticsCharts,
+  DiaryView
+};
